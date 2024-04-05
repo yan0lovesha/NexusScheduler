53ed76b882787acad6fc0da130a6849b02289b78
Ling Yan <neil.yan@outlook.com>
2024-04-04 20:10:44 -0700

    add code

---
+++ Playwright/Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Playwright;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+[DllImport("User32.dll", CharSet = CharSet.Unicode)]
+static extern int MessageBox(IntPtr h, string m, string c, int type);
+
+Console.WriteLine("Start checking availabilities!");
+
+// Load the settings from the appsettings.json file.
+var settingsContent = File.ReadAllText("appsettings.json");
+var settings = JsonDocument.Parse(settingsContent)!;
+var username = settings.RootElement.GetProperty("UserName").GetString()!;
+var password = settings.RootElement.GetProperty("Password").GetString()!;
+var numberOfAppointments = settings.RootElement.GetProperty("NumberOfAppointments").GetInt32();
+var alertBeforeDate = settings.RootElement.GetProperty("AlertBeforeDate").GetDateTime();
+var refreshFrequencyInSecond = settings.RootElement.GetProperty("RefreshFrequencyInSecond").GetInt32();
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+{
+    Headless = false
+});
+
+// Try load the state from the previous run to avoid the MFA process.
+var stateExists = File.Exists("state.json");
+var context = stateExists switch
+{
+    true => await browser.NewContextAsync(new BrowserNewContextOptions
+    {
+        StorageStatePath = "state.json",
+        ScreenSize = new() { Width = 2560, Height = 1440 },
+        ViewportSize = new() { Width = 2560, Height = 1440 }
+    }),
+    false => await browser.NewContextAsync(new BrowserNewContextOptions
+    {
+        ScreenSize = new() { Width = 2560, Height = 1440 },
+        ViewportSize = new() { Width = 2560, Height = 1440 }
+    })
+};
+
+var page = await context.NewPageAsync();
+
+await page.GotoAsync("https://ttp.cbp.dhs.gov/");
+await page.GetByRole(AriaRole.Button, new() { Name = "Log In" }).ClickAsync();
+await page.GetByRole(AriaRole.Button, new() { Name = "CONSENT & CONTINUE" }).ClickAsync();
+
+var signInButton = page.GetByRole(AriaRole.Button, new() { Name = "Sign in" });
+var dashboardTitle = page.GetByRole(AriaRole.Heading, new() { Name = "Dashboard" });
+var needSignIn = (await WaitForEitherTask(signInButton.WaitForAsync(), dashboardTitle.WaitForAsync()) == 0) ? true : false;
+if (needSignIn)
+{
+    await SignIn(page, username, password);
+    await dashboardTitle.WaitForAsync();
+}
+
+
+// Save the state to avoid the MFA process next time.
+await context.StorageStateAsync(new()
+{
+    Path = "state.json"
+});
+
+// I already have an appointment. So, I wait for the reschedule button to show up. 
+// You can edit this logic to fit your own situation.
+await page.GetByRole(AriaRole.Button, new() { Name = "reschedule" }).ClickAsync();
+
+// Set the number of appointments to the desired number.
+await page.GetByLabel("# of appts.").SelectOptionAsync(numberOfAppointments.ToString());
+await page.Locator("#centerDetailsUS70").ClickAsync();
+await page.GetByRole(AriaRole.Button, new() { Name = "Choose This Location" }).ClickAsync();
+
+await page.GetByLabel("# of appts.").SelectOptionAsync(numberOfAppointments.ToString());
+
+// Keep apply the filter to refresh the availability.
+while (true)
+{
+    await page.GetByRole(AriaRole.Button, new() { Name = "Apply" }).ClickAsync();
+    var responseForOne = page.WaitForResponseAsync(new Regex(@"slots\?orderBy=soonest&limit=1"));
+    var responseForAll = page.WaitForResponseAsync(new Regex(@"slots\?startTimestamp"));
+    await Task.WhenAll(responseForOne, responseForAll);
+
+    var responseContent = await responseForOne.Result.BodyAsync();
+    var responseString = Encoding.UTF8.GetString(responseContent);
+    var responseJson = JsonSerializer.Deserialize<JsonElement>(responseString);
+    var availabaility = responseJson.EnumerateArray().First().GetProperty("startTimestamp").GetDateTime();
+    if (availabaility < alertBeforeDate)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        
+        // The message box will show up and block the code execution until you close it.
+        MessageBox((IntPtr)0, availabaility.ToString(), "It's available", 0);
+    }
+    Console.WriteLine($"The first availability is on {availabaility}");
+    Console.ResetColor();
+
+    await page.WaitForTimeoutAsync(refreshFrequencyInSecond);
+    await context.StorageStateAsync(new()
+    {
+        Path = "state.json"
+    });
+}
+
+static async Task SignIn(IPage page, string username, string password)
+{
+    await page.GetByLabel("Email address").ClickAsync();
+    await page.GetByLabel("Email address").ClickAsync();
+    await page.GetByLabel("Email address").FillAsync(username);
+    await page.GetByLabel("Email address").ClickAsync();
+    await page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+    await page.GetByLabel("Password", new() { Exact = true }).FillAsync(password);
+    await page.GetByRole(AriaRole.Button, new() { Name = "Sign in" }).ClickAsync();
+}
+
+static async Task<int> WaitForEitherTask(Task task1, Task task2)
+{
+    Task completedTask = await Task.WhenAny(task1, task2);
+
+    if (completedTask == task1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # NexusScheduler
 This tool automatically refreshes the Nexus scheduler to help you find an earlier interview slot.
+
+You might need to change some code to make it work for you. It was implemented pretty quick for my own scenario.
+
+I already have appointment booked. I just want to reschedule it to be sooner if any availibilities. 
+
+So, the code was trying to click the reschedule button to get into the scheduler page.
+
+My interview has been done. So, I am not able to use my account to inspect the page to make the code more general.
+
+ **If you don't have any appointment booked, you might need to change the code for your scenario. Or you could book a slot first, then use this tool.**
+
+This tool is built on [Playwright](https://playwright.dev/dotnet/docs/intro]) for the browser automation. You need to install the required broswer driver before running the tool. You can also go to the Playwright document page to learn how to customize the code for your scenario.
+
+#### Prerequisites
+- dotnet sdk. Suggesnt 8.0 or later. Download it from [here](https://dotnet.microsoft.com/en-us/download)
+
+#### How to use the tool
+1. Build the project locally by using Visual Studio or running the following command in the root folder:
+```
+dotnet build
+```
+There will be a powershell file generated in the output folder. The path should looks like NexusScheduler\Playwright\bin\Debug\net8.0\playwright.ps1. If you are using a different dotnet SDK, the net8.0 folder name will be different The file name is `playwright.ps1`.
+
+2. Install the browser driver by running the following command:
+```
+playwright.ps1 install
+```
+
+#### How to run the tool
+1. Open the `appsettings.json` file and update the following fields:
+   - `UserName`: Your Nexus username.
+   - `Password`: Your Nexus password.
+   - `NumberOfAppointments`: The number of appointments you want to book. This is not that important because even there is only one appointment, you probably still want to take it.
+   - `AlertBeforeDate`: This could help you ignore the availibilities after this date. If there is a slot available before this date, the tool will popup an alert window and show some text in the console window in green. The date format is `yyyy-MM-dd`.
+   - `RefreshFrequencyInSecond`: How many seconds the tool will wait before refreshing the page. The default value is 5 seconds. **I don't suggest to set this value too low because it may cause the tool to be blocked by the Nexus server.**
+
+2. Run the Playwright.exe file in the Playwright output folder(NexusScheduler\Playwright\bin\Debug\net8.0\).
+3. The tool will open a browser window and start from signing in by using the credential you configured in the `appsettings.json` file. For the first time signing in, **you will see a page like blow that ask you input a code** from text message to your phone number. Please manually input it. Next time when you run the tool again, this step will be skipped.
+
+![image](https://github.com/yan0lovesha/NexusScheduler/assets/28606510/7b213c11-af54-43cb-a981-bf41a75ef719)
+
+4. After the tool start to refresh the page, you could just let it go and do your own work on your computer. When there is an earlier slot available, the tool will popup an alert window and show some green text in the console window in green.
+The popup alert window may be covered by your other windows. But you should see a new icon shows up on your task bar. You need to pay attention to it.
+
+5. Once you saw the alert, you can go to the refreshing browser window and continue book the slot manually.
+
+6. After you booked the slot, you could close the browser window. Update the settings again to book a even ealier slot.
+**Do not expect to get a slot on tomorrow from your first try. You should alway try to just get a slot earlier than your current one. Then keep using the tool to get a even earlier slot.**
+
+7. You might need to re-run the tool when you get a chance, because sometimes your credential will be expired after maybe 30 minutes. Even the tool detect a slot, you need to sign-in again to book it. The sign-in process may cause you lost the slot if some other took it earlier than you. I have to sing-in again for 3 times, but eventually I still got the slots after I sign-in again. So, don't be frastrated on re-run the tool. If you have programming knowledge, you can change the code to auto sign-in again every 30 minutes.
+
+
